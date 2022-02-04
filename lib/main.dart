f363fb0fc5cbcadc2da524ec5b49e97f5b1a758f
Mouaad Aallam <mouaad.aallam@algolia.com>
2022-02-04 16:25:18 +0100

    add appbar basic ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/ui/home.dart';
 import 'package:my_app/ui/search.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(SWApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class SWApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Algolia & Flutter',
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-      ),
-      home: SearchScreen(title: 'Algolia & Flutter'),
+      title: 'Spencer & Williams Fashion',
+      theme: ThemeData.light(),
+      home: HomeScreen(),
     );
   }
 }
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,7 +9,54 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+        appBar: AppBar(
+          title: Image.asset('assets/images/og.png'),
+          actions: [
+            IconLabel(Icons.pin_drop_outlined, 'STORES'),
+            IconLabel(Icons.person_outline, 'ACCOUNTS'),
+            IconLabel(Icons.shopping_bag_outlined, 'CART')
+          ],
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(48.0),
+            child: Row(
+              children: [
+                SizedBox(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Row(
+                      children: [Icon(Icons.menu), Text("Menu")],
+                    ),
+                  ),
+                ),
+                Flexible(
+                  child: TextField(
+                    enabled: false,
+                    decoration: InputDecoration(
+                      suffixIcon: Icon(Icons.search)
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+        body: SafeArea(child: Center(child: Text("Hello world"))));
+  }
+
+  Column IconLabel(IconData? icon, String text) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Icon(icon),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(2.0),
+          child: Text(text),
+        ),
+      ],
+    );
   }
 }
