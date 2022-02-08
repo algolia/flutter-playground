a480cd161ae1a71cfa6c54c88ffebd85e267e235
Mouaad Aallam <mouaad.aallam@algolia.com>
2022-02-08 11:33:04 +0100

    add banner

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -71,16 +71,43 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Column(
           children: [
             Stack(
+              alignment: AlignmentDirectional.centerStart,
               children: [
                 Container(
                   alignment: Alignment.center,
-                  child: Image.asset(
-                    'assets/images/banner.jpg',
-                    height: 128,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
+                  child: ColorFiltered(
+                    colorFilter: ColorFilter.mode(
+                        Colors.black.withOpacity(0.33), BlendMode.srcOver),
+                    child: Image.asset(
+                      'assets/images/banner.jpg',
+                      height: 128,
+                      width: MediaQuery.of(context).size.width,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('NEW\nCOLLECTION',
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline4
+                              ?.copyWith(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white)),
+                      Text('SPRING/SUMMER 2022',
+                          style: Theme.of(context)
+                              .textTheme
+                              .subtitle2
+                              ?.copyWith(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white)),
+                    ],
+                  ),
+                )
               ],
             ),
           ],
