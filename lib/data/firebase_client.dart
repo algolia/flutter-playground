93f53a21e03ccf896dc01d33089faabafa6b8db6
Mouaad Aallam <mouaad.aallam@algolia.com>
2022-02-18 10:53:30 +0100

    refactor data repos

--- lib/data/firebase_client.dart
+++ lib/data/firebase_client.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_ecom_demo/model/product.dart';
 
+/// Firebase data repository.
 class FirebaseClient {
   FirebaseClient._internal();
 
@@ -13,6 +14,7 @@ class FirebaseClient {
   CollectionReference products =
       FirebaseFirestore.instance.collection('products');
 
+  /// Get a product by ID.
   Future<Product> get(String productID) async {
     final snapshot = await products.doc(productID).get();
     return Product.fromJson(snapshot.data() as Map<String, dynamic>);
